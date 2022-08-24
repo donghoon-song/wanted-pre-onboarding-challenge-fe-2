c90da21ebdd1f19ee007d7d04f9346b6867375c4
Donghoon <thdehdgns@gmail.com>
2022-08-25 08:42:09 +0900

    Todo 정의 추가

---
+++ src/Model/todo.js
@@ -0,0 +1,15 @@
+/**
+ * 할일
+ * @typedef {Object} Todo
+ * @property {number} id - 할일 id
+ * @property {string} content - 할일 내용
+ * @property {boolean} completed - 할일 완료 여부
+ * @property {string} category - 카테고리
+ * @property {string[]} [tags] - 태그들
+ */
+
+/**
+ * 할일 목록
+ * @type {Todo[]}
+ */
+const todo = [];
